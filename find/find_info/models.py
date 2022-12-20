257a73f456227df5d2f36fe779fdb9c9e7b37398
User <lg2031832@gmail.com>
2022-12-20 13:35:47 +0200

    fix

--- find/find_info/models.py
+++ find/find_info/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Document(models.Model):
+    objects = None
     name = models.CharField(max_length=100, null=True, blank=True)
     file = models.FileField()
 
@@ -12,11 +13,12 @@ class Doc(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата оновлення')
     link = models.CharField(max_length=150, verbose_name='Посилання')
     file = models.FileField(null=True)
-    description = models.CharField(max_length=150, verbose_name='Опис')
+    description = models.TextField(blank=True, verbose_name='Текст')
+    about_file = models.CharField(max_length=200, verbose_name='Опис', blank=True)
 
     def __str__(self):
         return self.name
 
     class Meta:
         verbose_name = 'Doc'
-        ordering = ['description', 'name', 'updated_at', 'link']
+        ordering = ['about_file', 'name', 'updated_at', 'link']
--- find/find_info/views.py
+++ find/find_info/views.py
@@ -7,19 +7,37 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 def document_save(request):
     if request.method == "POST":
         doc = request.FILES["file"]
-        link = "D:\\Users\\User\\Have_to\\one\\find\\media\\" + doc.name
-        doc_file = Doc.objects.create(name=doc.name, file=doc, link=link)
+
+        doc_file = Document.objects.create(name=doc.name, file=doc)
+        doc_path = doc_file.file.path
+        doc_type = doc.name.split('.')
+        doc_type = doc_type[-1]
+
+        description = []
+        with open(doc_path, 'r', encoding="utf-8") as file:
+            for line in file:
+                words = line.strip().split(' ')
+                description.append(words)
+
+        about_file = ''
+
+        if len(description) != 0:
+            for i in description[0]:
+                about_file += i + ' '
+
+        doc_file = Doc.objects.create(name=doc.name, file=doc, link=doc_path,
+                                      description=description, about_file=about_file)
         doc_path = doc_file.file.path
         return render(request, "main/test.html", {"doc_path": doc_path})
     return render(request, "main/test.html")
 
 
 def index(request):
-    checks_list = Doc.objects.all()
+    checks_list = Doc.objects.order_by('-id')
     query = request.GET.get('q')
     if query:
         checks_list = Doc.objects.filter(
-            Q(name__icontains=query)).distinct()
+            Q(name__icontains=query) | Q(description__icontains=query)).distinct()
 
     paginator = Paginator(checks_list, 5)  # 5 posts per page
     page = request.GET.get('page')
