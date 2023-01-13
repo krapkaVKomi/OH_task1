dc2e5d098dfe8871379ffbe0a2411a0ba91696cc
User <lg2031832@gmail.com>
2023-01-13 17:25:17 +0200

    lang fix

--- find/find_info/forms.py
+++ find/find_info/forms.py
@@ -4,8 +4,8 @@ from django.contrib.auth.forms import AuthenticationForm
 
 
 class UserLoginForm(AuthenticationForm):
-    username = forms.CharField(label="Логін", widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(label="Пароль", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(label="Login", widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
 
 class NameForm(forms.Form):
