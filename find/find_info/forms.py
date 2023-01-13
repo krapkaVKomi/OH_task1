b3a0ecf0b43121587ca2027709815b57a0306a6a
User <lg2031832@gmail.com>
2023-01-14 01:54:36 +0200

    fix modal wnd

--- find/find_info/forms.py
+++ find/find_info/forms.py
@@ -9,4 +9,4 @@ class UserLoginForm(AuthenticationForm):
 
 
 class NameForm(forms.Form):
-    your_name = forms.CharField(label='Your name', max_length=100)
+    formFile = forms.CharField(label='Your name', max_length=100)
