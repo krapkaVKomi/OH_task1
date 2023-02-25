02be74decc9489e65406b6c0506ca5fd9c7dcfa7
krapkaVKomi <lohvyn.oleh@gmail.com>
2023-02-25 03:18:34 +0200

    print(request.POST.get('fileExtension'))

--- find/find_info/views.py
+++ find/find_info/views.py
@@ -400,7 +400,7 @@ def my_view(request):
         for item in docs:
             doc_names.append(item.name)
         test_input = request.POST.get('testInput')  # Read value of text input
-
+        print(request.POST.get('fileExtension'))
         if test_input in doc_names:
             data = {'error': 'This file name is already used, try another one!'}
 
